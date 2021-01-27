6237b5f62930fabcf65c5ffdce127b88630489ba
Sayeed Afridi <as.afridi563@gmail.com>
2021-01-27 21:30:13 +0600

    class: 03 simple api and templating

--- 01. lSpec/index.js
+++ 01. lSpec/index.js
@@ -1,12 +1,37 @@
 const http = require('http');
+const fs = require('fs');
+
+const dataString = fs.readFileSync('data.json', 'utf-8');
+const data = JSON.parse(dataString);
+const overView = fs.readFileSync('templates/overview_template.html', 'utf-8');
+const cardTemplate = fs.readFileSync('templates/card_template.html', 'utf-8');
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if(pathName === '/overview' || pathName === '/'){
-    res.end('Hello from overview!');
-  }else if(pathName === '/laptop'){
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    })
+    const cards = data.map(mobile => {
+      let card = cardTemplate;
+      card = card.replace('%image%', mobile.image);
+      card = card.replace('%name%', mobile.name);
+      card = card.replace('%ram%', mobile.ram);
+      card = card.replace('%rom%', mobile.rom);
+      card = card.replace('%id%', mobile.id);
+      card = card.replace('%price%', mobile.price);
+      return card
+    })
+    const template = overView.replace('%product-cards%', cards.join(''))
+    res.end(template)
+  }else if(pathName === '/mobile'){
     res.end('Hello from laptop page!');
-  } else {
+  } else if(pathName === '/api'){
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    })
+    res.end(dataString);
+  }else {
     res.writeHead(404, {
       'Content-type': 'text/html',
       'my-own-header': 'This is a test'
@@ -16,5 +41,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(1300, '127.0.0.1', () => {
-  console.log(`Server listening on port 1300`)
+  console.log(`Server listening on port 1300`);
 })
